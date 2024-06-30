76c95a0a753e25507cf5f6951804054a85d3a57e
ayrton <ayrtonundec@gmail.com>
2024-06-29 21:34:50 -0300

    carga de package json y home modificado

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const path = require("node:path");
 
 app.use(express.static("./public"));
 
-
-app.listen(PORT,()=> console.log(`Servidor corriendo en puerto ${3030}`));
+const PORT = 3030;
+app.listen(PORT,()=> console.log(`Servidor corriendo en puerto ${PORT}`));
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve("views/home.html"));
