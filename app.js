97d2f7fbf2963e41557cd5f0411e507605f8589c
ayrton <ayrtonundec@gmail.com>
2024-07-02 15:16:20 -0300

    agregado de login y register y modificaciones en css

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const app = express();
 const path = require("node:path");
-
-app.use(express.static("./public"));
-
 const PORT = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.listen(PORT,()=> console.log(`Servidor corriendo en puerto ${PORT}`));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve("views/home.html"));
- });
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve (__dirname, "./views/home.html"));
+});
+
+ app.get("/register", (req, res) => {
+    res.sendFile(path.resolve (__dirname, "./views/register.html"));
+});
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.resolve (__dirname, "./views/login.html"));
+});
